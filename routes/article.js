d2ff171cc47289982dda35d943e6af75dff7abba
曹凱嵐Jacky <kaij1941@gmail.com>
2022-05-18 23:15:25 +0800

    feat:整理發文程式碼及刪除程式碼(加上登入頁)和增加controller分層

--- routes/article.js
+++ routes/article.js
@@ -3,57 +3,34 @@ const { json } = require('express/lib/response');
 var router = express.Router();
 var article =require('.././models/newArticlePost');
 const { isAuth } = require('../middleware/auth');
+const articleController = require('../controller/article')
+const handleErrorAsync = require("../service/handleErrorAsync");
 //var article =require('.././models/Article');
 /**
  * 新增貼文
  */
-router.post('/',async function(req,res){      
-    if (! req.body.content) {
-        throw  new Error("貼文內容為必填!");
-    }
-    //預設讀取登入者資料
-    let userId ="0";
-    if(! userId){
-        throw new Error("請先登入在填寫!"); 
-    }
-
-    let imageId =req.body.imageId
-    if (! imageId) {
-        imageId = "";
-    }
-
-    await article.create({
-        "content":req.body.content,
-        "userId":userId,
-        "imageId": imageId
-    });
-
-    res.send('新增成功!');
-})
+router.post(
+    '/',
+    isAuth,
+    handleErrorAsync(async (req,res,next)=> 
+    articleController.createPosts(req,res,next))
+)
 
 //檢測用
-router.get('/',async function(req,res){      
-   let result = await article.find();
-
-    res.send(result);
-})
+router.get(
+    '/',
+    handleErrorAsync( async (req,res,next)=>
+    articleController.getAll(req,res,next)
+))
 
 /**
  * 刪除貼文
  */
-router.delete('/:postId',async function(req,res){
-    if(!req.params.postId){
-        throw new Error("請填寫要刪除的貼文")
-    }
-    let postId = req.params.postId;
-    
-    await article.findOneAndUpdate(
-    postId      
-    ,{
-        "isActive":false
-    })
-
-    res.send('刪除成功')
-} )
+router.delete(
+    '/:postId',
+    isAuth,
+    handleErrorAsync( async(req,res,next)=>
+    articleController.deletePosts(req,res,next))
+)
 
 module.exports = router;
