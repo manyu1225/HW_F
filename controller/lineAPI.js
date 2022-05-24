5ee2048217a0edb7704d184fe2cc5c8dca9479a3
manyu1225 <manyu1225@gmail.com>
2022-05-24 21:40:37 +0800

    add

--- controller/lineAPI.js
+++ controller/lineAPI.js
@@ -5,76 +5,29 @@ const handleErrorAsync = require("../service/handleErrorAsync");
 const appError = require("../service/appError");
 const qs = require("qs");
 const axios = require("axios");
+const jsonwebtoken = require("jsonwebtoken");
 //一、取得授權碼 code https://access.line.me/oauth2/v2.1/authorize
 //二、以回傳的授權碼再向 Line 取用戶資料 POST https://api.line.me/oauth2/v2.1/token
 
 const lineAPIController = {
-  async getAuthorizeUrl(req, res, next) {
-    const client_id = "1657154166";
+  async authorize(req, res, next) {
     const response_type = "code";
-    const state = "123123";
     const scope = "openid%20profile%20email"; //URL += 'profile';
-    const redirect_uri = "https://intense-fortress-59028.herokuapp.com/";
     let url =
-      "https://access.line.me/oauth2/v2.1/authorize" +
+      process.env.authorization_endpoint +
       "?response_type=" +
       response_type +
       "&client_id=" +
-      client_id +
-      // process.env.client_id +
+      process.env.client_id +
       "&redirect_uri=" +
-      encodeURIComponent(redirect_uri) +
+      encodeURIComponent(process.env.redirect_uri) +
       "&state=" +
-      state +
+      process.env.state +
       "&scope=" +
       scope;
-    console.log("state unmatch!" + url);
     res.redirect(url);
-    //   handleSuccess(res, httpStatus.OK, url);
-  },
-  async getLinetoken(req, res, next) {
-    const data = req.body;
-    const { code } = data;
-    const client_id = "1657154166";
-    const client_secret = "f6603d07c801c0631bf0306a8058a24f";
-    const payload = {
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: encodeURIComponent(
-        "https://intense-fortress-59028.herokuapp.com/"
-      ),
-      client_id: client_id,
-      client_secret: client_secret,
-    };
-    const resp = axios.post(
-      "https://api.line.me/oauth2/v2.1/token",
-      qs.stringify(payload)
-    );
-    handleSuccess(res, httpStatus.OK, resp);
-  },
-  async getLineUserInfo(req, res, next) {
-    const url = "https://api.line.me/oauth2/v2.1/verify";
-    const client_id = "1657154166";
-    const data = req.body;
-    const { id_token } = id_token;
-    const resp = axios.post(
-      url,
-      qs.stringify({
-        client_id: client_id,
-        id_token: id_token,
-      }),
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        },
-      }
-    );
-    handleSuccess(res, httpStatus.OK, resp);
   },
   async cb(req, res, next) {
-    const client_id = "1657154166";
-    const client_secret = "f6603d07c801c0631bf0306a8058a24f";
-    const redirect_uri = "https://intense-fortress-59028.herokuapp.com/cb";
     res.send(
       "<html><body>" +
         '<form method="post" action="/token">' +
@@ -83,27 +36,27 @@ const lineAPIController = {
         req.query.code +
         '"></td></tr>' +
         '<tr><th>redirect_uri</th><td><input type="text" name="redirect_uri" size="100" value="' +
-        redirect_uri +
+        process.env.redirect_uri +
         '"></td></tr>' +
         '<tr><th>client_id</th><td><input type="text" name="client_id" size="100" value="' +
-        client_id +
+        process.env.client_id +
         '"></td></tr>' +
         '<tr><th>client_secret</th><td><input type="text" name="client_secret" size="100" value="' +
-        client_secret +
+        process.env.client_secret +
         '"></td></tr>' +
         '</table><button type="submit">Exchange code to token</button><br>' +
         "</form></body></html>"
     );
   },
-  async gettoken(req, res, next) {
-    "https://api.line.me/oauth2/v2.1/token",
+  async getLinetoken(req, res, next) {
+    process.env.token_endpoint,
       {
         form: {
           grant_type: "authorization_code",
           code: req.body.code,
-          redirect_uri: redirect_uri,
-          client_id: client_id,
-          client_secret: client_secret,
+          redirect_uri: process.env.redirect_uri,
+          client_id: process.env.client_id,
+          client_secret: process.env.client_secret,
         },
       },
       function (e, r, body) {
@@ -116,8 +69,6 @@ const lineAPIController = {
               client_secret
             );
             // verify nonce in jwt
-
-            // show form
             res.send(
               "<html><body>" +
                 '<form method="post" action="/userInfo">' +
@@ -153,6 +104,38 @@ const lineAPIController = {
           console.log(body);
         }
       };
+  },
+  async getLineUserInfo(req, res, next) {
+    request.get(
+      process.env.profile_endpoint,
+      {
+        headers: {
+          Authorization: "Bearer " + req.body.access_token,
+        },
+      },
+      function (e, r, body) {
+        if (!e && r.statusCode == 200) {
+          var jsonBody = JSON.parse(body);
+          res.send(
+            "<html><body>" +
+              '<table border="1"><tr><th>userId</th><td>' +
+              jsonBody.userId +
+              "</td></tr>" +
+              "<tr><th>displayName</th><td>" +
+              jsonBody.displayName +
+              "</td></tr>" +
+              "<tr><th>pictureUrl</th><td>" +
+              jsonBody.pictureUrl +
+              "</td></tr>" +
+              "</table>" +
+              "</body></html>"
+          );
+        } else {
+          res.send("error");
+          console.log(body);
+        }
+      }
+    );
   },
   //grant_type=authorization_code&code=DYHBuiE7ujY09oweyYAM&redirect_uri=https%253A%252F%252Fintense-fortress-59028.herokuapp.com%252F&client_id=1657154166&client_secret=f6603d07c801c0631bf0306a8058a24f
   /* let queryData = this.$route.query;
--- routes/lineAPI.js
+++ routes/lineAPI.js
@@ -5,21 +5,10 @@ const handleErrorAsync = require("../service/handleErrorAsync");
 router.get(
   "/line/authorize",
   handleErrorAsync(async (req, res, next) =>
-    lineAPIController.getAuthorizeUrl(req, res, next)
+    lineAPIController.authorize(req, res, next)
   )
 ); //https://access.line.me/oauth2/v2.1/ssoLogin?loginChannelId=1657154166&returnUri=%2Foauth2%2Fv2.1%2Fauthorize%2Fconsent%3Fscope%3Dopenid%2Bprofile%2Bemail%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fintense-fortress-59028.herokuapp.com%252F%26state%3D123123%26client_id%3D1657154166
-router.post(
-  "/line/token",
-  handleErrorAsync(async (req, res, next) =>
-    lineAPIController.getLinetoken(req, res, next)
-  )
-); //https://intense-fortress-59028.herokuapp.com/?code=MtKd8W32KlL4trPnt18N&state=123123
-router.post(
-  "/line/userInfo",
-  handleErrorAsync(async (req, res, next) =>
-    lineAPIController.getLineUserInfo(req, res, next)
-  )
-);
+// callback
 router.get(
   "/cb",
   handleErrorAsync(async (req, res, next) =>
@@ -29,7 +18,14 @@ router.get(
 router.post(
   "/line/token",
   handleErrorAsync(async (req, res, next) =>
-    lineAPIController.gettoken(req, res, next)
+    lineAPIController.getLinetoken(req, res, next)
+  )
+); //https://intense-fortress-59028.herokuapp.com/?code=MtKd8W32KlL4trPnt18N&state=123123
+
+router.post(
+  "/line/userInfo",
+  handleErrorAsync(async (req, res, next) =>
+    lineAPIController.getLineUserInfo(req, res, next)
   )
 );
 module.exports = router;
