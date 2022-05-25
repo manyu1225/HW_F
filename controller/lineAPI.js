f50610864167a9d468c47a215dadd18738cc4643
manyu1225 <manyu1225@gmail.com>
2022-05-25 23:30:10 +0800

    a

--- controller/lineAPI.js
+++ controller/lineAPI.js
@@ -38,6 +38,8 @@ const lineAPIController = {
     const redirect_uri = process.env.redirect_uri;
     const code = req.query.code;
     const token_endpoint = process.env.token_endpoint;
+    console.log("code=>",    code;
+);
     axios
       .post(
         token_endpoint,
