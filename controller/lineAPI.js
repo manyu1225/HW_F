2ae2d5d6a9d074920f0c2132f7a90c389059ab6c
manyu1225 <manyu1225@gmail.com>
2022-05-28 01:01:50 +0800

    e

--- controller/lineAPI.js
+++ controller/lineAPI.js
@@ -5,7 +5,7 @@ const handleErrorAsync = require("../service/handleErrorAsync");
 const appError = require("../service/appError");
 const axios = require("axios");
 const jsonwebtoken = require("jwt-decode");
-
+const qs = require("qs");
 const lineAPIController = {
   async authorize(req, res, next) {
     const client_id = process.env.client_id;
@@ -19,50 +19,77 @@ const lineAPIController = {
       "&redirect_uri=" +
       encodeURIComponent(redirect_uri) +
       "&state=" +
-      state +
+      process.env.state +
       "&scope=" +
       scope;
     res.redirect(url);
   },
   async callback(req, res, next) {
     console.log("code=======>", req.query.code);
-    let reqPramater =
-      "grant_type=authorization_code&code=" +
-      req.query.code +
-      "&redirect_uri=" +
-      process.env.redirect_uri +
-      "&client_id=" +
-      process.env.client_id +
-      "&client_secret=" +
-      process.env.client_secret;
-    axios.post(process.env.token_endpoint, reqPramater).then(function (resp) {
-      console.log("resp.data=>", resp.data);
-      let decoded = jsonwebtoken(resp.data.id_token);
-      console.log(decoded.email);
-      resp.data.email = decoded.email;
+
+    if (!req.query.code) {
       handleSuccess(res, httpStatus.OK, resp.data);
-    });
+      console.log("=======>state unmatch!");
+    } else {
+      let reqPramater = qs.stringify({
+        grant_type: "authorization_code",
+        code: req.query.code,
+        redirect_uri: encodeURIComponent(process.env.redirect_uri),
+        client_id: process.env.client_id,
+        client_secret: process.env.client_secret,
+      });
+      axios
+        .post(process.env.token_endpoint, reqPramater, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        })
+        .then(function (res) {
+          console.log("resp.data=>", res.data);
+          let decoded = jsonwebtoken(res.data.id_token);
+          console.log("decoded.email=>", decoded.email);
+          res.data.email = decoded.email;
+
+          handleSuccess(res, httpStatus.OK, res.data);
+        });
+    }
+    return;
   },
   async getLinetoken(req, res, next) {
-    axios
-      .post(
-        process.env.token_endpoint,
-        "grant_type=authorization_code&code=" +
-          req.body.code +
-          "&redirect_uri=" +
-          process.env.redirect_uri +
-          "&client_id=" +
-          process.env.client_id +
-          "&client_secret=" +
-          process.env.client_secret
-      )
-      .then(function (resp) {
-        console.log("res_Token=>", resp.data);
-        let decoded = jsonwebtoken(resp.data.id_token);
-        console.log(decoded.email);
-        resp.data.email = decoded.email;
-        handleSuccess(res, httpStatus.OK, resp.data);
+    console.log("getLinetoken====>", req.body.code);
+
+    if (!req.body.code) {
+      console.log("=======>state unmatch!");
+      return appError(httpStatus.BAD_REQUEST, "ERR...", next);
+    } else {
+      let reqPramater = qs.stringify({
+        grant_type: "authorization_code",
+        code: req.query.code,
+        redirect_uri: encodeURIComponent(process.env.redirect_uri),
+        client_id: process.env.client_id,
+        client_secret: process.env.client_secret,
       });
+      console.log("reqPramater=======>", reqPramater);
+
+      axios
+        .post(process.env.token_endpoint, reqPramater, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        })
+        .then(function (resp) {
+          console.log("resp.data=>", resp.data);
+          let decoded = jsonwebtoken(resp.data.id_token);
+          console.log("decoded.email=>", decoded.email);
+          resp.data.email = decoded.email;
+          handleSuccess(res, httpStatus.OK, resp.data);
+        })
+        .catch((e) => {
+          console.log(e);
+          return appError(httpStatus.BAD_REQUEST, "ERR.", next);
+        });
+    }
+    return appError(httpStatus.BAD_REQUEST, "ERR..", next);
   },
   async getLineUserInfo(req, res, next) {
     axios
