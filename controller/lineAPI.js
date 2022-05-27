3a598ef5afe66da1d6930324f7b1b8a75f30ca27
manyu1225 <manyu1225@gmail.com>
2022-05-28 01:01:50 +0800

    e

--- controller/lineAPI.js
+++ controller/lineAPI.js
@@ -5,6 +5,8 @@ const handleErrorAsync = require("../service/handleErrorAsync");
 const appError = require("../service/appError");
 const axios = require("axios");
 const jsonwebtoken = require("jwt-decode");
+//一、取得授權碼 code https://access.line.me/oauth2/v2.1/authorize
+//二、以回傳的授權碼再向 Line 取用戶資料 POST https://api.line.me/oauth2/v2.1/token
 
 const lineAPIController = {
   async authorize(req, res, next) {
@@ -19,14 +21,14 @@ const lineAPIController = {
       "&redirect_uri=" +
       encodeURIComponent(redirect_uri) +
       "&state=" +
-      state +
+      process.env.state +
       "&scope=" +
       scope;
     res.redirect(url);
   },
   async callback(req, res, next) {
     console.log("code=======>", req.query.code);
-    let reqPramater =
+    const reqPramater =
       "grant_type=authorization_code&code=" +
       req.query.code +
       "&redirect_uri=" +
@@ -37,7 +39,7 @@ const lineAPIController = {
       process.env.client_secret;
     axios.post(process.env.token_endpoint, reqPramater).then(function (resp) {
       console.log("resp.data=>", resp.data);
-      let decoded = jsonwebtoken(resp.data.id_token);
+      var decoded = jsonwebtoken(resp.data.id_token);
       console.log(decoded.email);
       resp.data.email = decoded.email;
       handleSuccess(res, httpStatus.OK, resp.data);
@@ -47,7 +49,7 @@ const lineAPIController = {
     axios
       .post(
         process.env.token_endpoint,
-        "grant_type=authorization_code&code=" +
+        "?grant_type=authorization_code&code=" +
           req.body.code +
           "&redirect_uri=" +
           process.env.redirect_uri +
@@ -58,7 +60,8 @@ const lineAPIController = {
       )
       .then(function (resp) {
         console.log("res_Token=>", resp.data);
-        let decoded = jsonwebtoken(resp.data.id_token);
+        //  console.log("decoded=>", decoded);
+        var decoded = jsonwebtoken(resp.data.id_token);
         console.log(decoded.email);
         resp.data.email = decoded.email;
         handleSuccess(res, httpStatus.OK, resp.data);
