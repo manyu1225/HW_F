f26dc4830709bba90ed923477b3f941bb4081fd1
曹凱嵐Jacky <kaij1941@gmail.com>
2022-06-05 01:32:20 +0800

    feat: 調整篩選邏輯(content為""或是''時為取全部) feat:修正案讚列表的顯示

--- models/ArticlePost.js
+++ models/ArticlePost.js
@@ -50,8 +50,9 @@ newArticleSchema.virtual("likeCount", {
   ref: "Likes",
   localField: "_id",
   foreignField: "post",
+  select:"_id",
   justOne: false,
-  count: true,
+  count: false,
 });
 
 newArticleSchema.virtual("comment", {
